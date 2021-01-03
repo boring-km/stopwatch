88438f8709373192b6ef843ef0961bba667cf198
boring-km <kms0644804@naver.com>
2021-01-03 20:01:56 +0900

    [master] 스탑워치 교재와 비스무레하게 완성

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(StopWatchApp());
 }
+
 // https://api.dart.dev/stable/2.10.4/dart-async/Timer-class.html
 class StopWatchApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-        home: MainPage()
-    );
+    return MaterialApp(home: MainPage());
   }
 }
 
@@ -23,16 +21,15 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   Timer _timer;
-  var _time = 0;
-  List<int> _data = [];
-  // FloatingActionButton(
-  //   child: Icon(Icons.pause),
-  //   onPressed: () {
-  //     setState(() {
-  //       _timer?.cancel();
-  //     });
-  //   },
-  // ),
+  var _ms = 0;
+  var _sec = 0;
+  List<String> _data = [];
+  var _onOff = false;
+  var _play = Icon(Icons.play_arrow);
+  var _rank = 0;
+
+  var _playColor = Colors.blue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,70 +37,115 @@ class _MainPageState extends State<MainPage> {
         title: Text('STOPWATCH'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('$_time'),
-            Container(
-                height: 300,
-                child: ListView(
-                  children: _data.map((time) => Text('$time')).toList(),
-                )
-            ),
-            Row(
-              children: [
-                Container(
-                  child: Align(
-                    alignment: Alignment.bottomLeft,
-                    child: RaisedButton(
-                      child: Text('초기화'),
-                      onPressed: (() {
-                        setState(() {
-                          setState(() {
-                            _data.clear();
-                            _timer?.cancel();
-                            _time = 0;
-                          });
-                        });
-                      }),
+          child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Stack(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            '$_sec',
+                            style: TextStyle(fontSize: 40),
+                          ),
+                          Text('$_ms'),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 400,
+                          child: ListView(
+                            children: _data.map((time) => Text(
+                              time,
+                              textAlign: TextAlign.center,
+                            )).toList(),
+                          )
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  Positioned(
+                    left: 10,
+                    right: 10,
+                    bottom: 30,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        buildResetButton(),
+                        buildPlayButton(),
+                        buildRecordButton(),
+                      ],
                     ),
                   ),
-                ),
-                Container(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: FloatingActionButton(
-                      child: Icon(Icons.play_arrow),
-                      onPressed: () {
-                        setState(() {
-                          _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
-                            setState(() {
-                              _time++;
-                            });
-                          });
-                        });
-                      },
-                    ),
-                  ),
-                ),
-                Container(
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: RaisedButton(
-                      child: Text('기록'),
-                      onPressed: () {
-                        setState(() {
-                          _data.insert(0, _time);
-                        });
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+                ],
+              ))),
+    );
+  }
+
+  RaisedButton buildRecordButton() {
+    return RaisedButton(
+      child: Text('랩타임'),
+      onPressed: () {
+        setState(() {
+          _rank++;
+          _data.insert(0, '$_rank등 $_sec.$_ms');
+        });
+      },
+    );
+  }
+
+  FloatingActionButton buildPlayButton() {
+    return FloatingActionButton(
+      child: _play,
+      backgroundColor: _playColor,
+      onPressed: () {
+        setState(() {
+          if (!_onOff) {
+            _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+              setState(() {
+                _onOff = true;
+                _play = Icon(Icons.pause);
+                _playColor = Colors.green;
+                _ms++;
+                if (_ms == 100) {
+                  _sec++;
+                  _ms = 0;
+                }
+              });
+            });
+          } else {
+            setState(() {
+              _onOff = false;
+              _play = Icon(Icons.play_arrow);
+              _playColor = Colors.blue;
+              _timer.cancel();
+            });
+          }
+        });
+      },
+    );
+  }
+
+  FloatingActionButton buildResetButton() {
+    return FloatingActionButton(
+      child: Icon(Icons.update_rounded),
+      backgroundColor: Colors.pink,
+      onPressed: () {
+        setState(() {
+          _data.clear();
+          _rank = 0;
+          _ms = 0;
+          _sec = 0;
+        });
+      },
     );
   }
 }
